a1363d4ce12621e8a1af71eee745e114f0c9ea89
ssy4230@naver.com <ssy4230@naver.com>
2021-02-12 21:31:45 +0900

    git shat

--- socket.js
+++ socket.js
@@ -1,29 +1,32 @@
 const SocketIO = require("socket.io");
 
-module.exports = (server) => {
+module.exports = (server, app) => {
   const io = SocketIO(server, { path: "/socket.io" });
+  app.set("io", io);
+  const room = io.of("/room");
+  const chat = io.of("/chat");
 
-  io.on("connection", (socket) => {
-    // 웹소켓 연결 시
-    const req = socket.request;
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("새로운 클라이언트 접속!", ip, socket.id, req.ip);
+  room.on("connection", (socket) => {
+    console.log("room 네임스페이스에 접속");
     socket.on("disconnect", () => {
-      // 연결 종료 시
-      console.log("클라이언트 접속 해제", ip, socket.id);
-      clearInterval(socket.interval);
+      console.log("room 네임스페이스 접속 해제");
     });
-    socket.on("error", (error) => {
-      // 에러 시
-      console.error(error);
+  });
+
+  chat.on("connection", (socket) => {
+    console.log("chat 네임스페이스에 접속");
+    const req = socket.request;
+    const {
+      headers: { referer },
+    } = req;
+    const roomId = referer
+      .split("/")
+      [referer.split("/").length - 1].replace(/\?.+/, "");
+    socket.join(roomId);
+
+    socket.on("disconnect", () => {
+      console.log("chat 네임스페이스 접속 해제");
+      socket.leave(roomId);
     });
-    socket.on("reply", (data) => {
-      // 클라이언트로부터 메시지
-      console.log(data);
-    });
-    socket.interval = setInterval(() => {
-      // 3초마다 클라이언트로 메시지 전송
-      socket.emit("news", "Hello Socket.IO");
-    }, 3000);
   });
 };
